f1bec1ac8adf1a136dbf8b3ec9d417eb8f9d0dc8
Кононыхин Ярослав Игоревич <ip917s08@awp05.csc.local>
2020-02-17 09:35:10 +0700

    add input func, add func of transtating the letter format into a numeric

--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
-#include <stdio.h>
-
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
 int main() {
   int i, j;
-
+  ifstream tf;
+  string  a;
+  tf.open("input.txt");
   char mas[8][8] = {{'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'},
                     {'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
                     {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
@@ -13,9 +17,30 @@ int main() {
                     {'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'}};
   for (i = 0; i < 8; ++i) {
     for (j = 0; j < 8; ++j) {
-      printf("%c ", mas[i][j]);
+      cout << mas[i][j];
     }
-    printf("\n");
+    cout << endl;
   }
+  getline(tf,a);
+  //cout << a;
+  int x1,x2,y1,y2;
+  bool oper;
+  char b;
+  int k = 0;
+  while(b!='-' && b!='x'){
+    b = a[k];
+    switch(b <= 'h' && b >= 'a'){
+    case true:
+      x1 = (int)('h' - b + 1);
+      cout << x1;
+      break;
+    }
+    switch(b <='8' && b >= '1'){
+    case true:y1 = int(b) - 48;cout << "-" << y1; break;
+    }
+	   
+    k++;
+  }
+  cout << endl;
 }
 
